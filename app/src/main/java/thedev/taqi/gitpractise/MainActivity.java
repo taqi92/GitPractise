5bebf79f7f5aad372ad32b909569e1d43966183f
Taqi Tahmid Tanzil <Taqi Tahmid Tanzil>
2017-05-23 12:37:38 +0600

    Initial commit

---
+++ app/src/main/java/thedev/taqi/gitpractise/MainActivity.java
@@ -0,0 +1,17 @@
+package thedev.taqi.gitpractise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testUpload(){
+        int i =1+1;
+    }
+}
